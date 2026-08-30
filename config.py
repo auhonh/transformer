6a5e54733d2437e68ed6b4fd884378bd717d4271
Auhon Haldar <auhonh@gmail.com>
2026-08-30 18:36:17 -0400

    more data, changed weights

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import torch
 
 batch_size = 64
 block_size = 256
-n_embd = 64
+n_embd = 128
 n_head = 4
-n_layer = 3
-dropout = 0.2
+n_layer = 4
+dropout = 0.15
 
 lr = 1e-3
-max_iters = 6000
-eval_interval = 300
+max_iters = 8000
+eval_interval = 400
 eval_iters = 100
 
 device = "mps" if torch.backends.mps.is_available() else ("cuda" if torch.cuda.is_available() else "cpu")
